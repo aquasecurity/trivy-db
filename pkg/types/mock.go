068be57f1b9e1ca7b7a9409f9d2677f7776bb492
knqyf263 <knqyf263@gmail.com>
2019-11-19 16:33:00 +0200

    test(vulnsrc): update mock

--- pkg/types/mock.go
+++ pkg/types/mock.go
@@ -6,12 +6,12 @@ type MockVulnSrc struct {
 	mock.Mock
 }
 
-func (_m MockVulnSrc) Update(a string) error {
+func (_m *MockVulnSrc) Update(a string) error {
 	ret := _m.Called(a)
 	return ret.Error(0)
 }
 
-func (_m MockVulnSrc) Get(a, b string) ([]Advisory, error) {
+func (_m *MockVulnSrc) Get(a, b string) ([]Advisory, error) {
 	ret := _m.Called(a, b)
 	ret0 := ret.Get(0)
 	if ret0 == nil {
