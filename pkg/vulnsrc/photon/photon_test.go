4350e1b911b74f9511e8107512e6bde160050ac0
knqyf263 <knqyf263@gmail.com>
2019-12-25 21:04:17 +0200

    test(photon): add tests

---
+++ pkg/vulnsrc/photon/photon_test.go
@@ -0,0 +1,279 @@
+package photon
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/aquasecurity/trivy-db/pkg/vulnsrc/vulnerability"
+
+	"github.com/aquasecurity/trivy-db/pkg/types"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/aquasecurity/trivy-db/pkg/db"
+)
+
+func TestVulnSrc_Update(t *testing.T) {
+	type args struct {
+		dir string
+	}
+	tests := []struct {
+		name        string
+		args        args
+		batchUpdate db.BatchUpdateExpectation
+		wantErr     string
+	}{
+		{
+			name: "happy path",
+			args: args{
+				dir: "testdata",
+			},
+			batchUpdate: db.BatchUpdateExpectation{
+				Args: db.BatchUpdateArgs{
+					FnAnything: true,
+				},
+			},
+		},
+		{
+			name: "cache dir doesnt exist",
+			args: args{
+				dir: "badpathdoesnotexist",
+			},
+			batchUpdate: db.BatchUpdateExpectation{
+				Args: db.BatchUpdateArgs{
+					FnAnything: true,
+				},
+			},
+			wantErr: "no such file or directory",
+		},
+		{
+			name: "BatchUpdate returns an error",
+			args: args{
+				dir: "testdata",
+			},
+			batchUpdate: db.BatchUpdateExpectation{
+				Args: db.BatchUpdateArgs{
+					FnAnything: true,
+				},
+				Returns: db.BatchUpdateReturns{
+					Err: errors.New("error"),
+				},
+			},
+			wantErr: "unable to save Photon advisories",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockDBConfig := new(db.MockOperation)
+			mockDBConfig.ApplyBatchUpdateExpectation(tt.batchUpdate)
+
+			vs := VulnSrc{
+				dbc: mockDBConfig,
+			}
+			err := vs.Update(tt.args.dir)
+			if tt.wantErr != "" {
+				require.NotNil(t, err, tt.name)
+				assert.Contains(t, err.Error(), tt.wantErr, tt.name)
+			} else {
+				assert.NoError(t, err, tt.name)
+			}
+		})
+	}
+}
+
+func TestVulnSrc_commit(t *testing.T) {
+	type args struct {
+		cves []PhotonCVE
+	}
+	tests := []struct {
+		name                   string
+		args                   args
+		putAdvisory            db.PutAdvisoryExpectation
+		putVulnerabilityDetail db.PutVulnerabilityDetailExpectation
+		putSeverity            db.PutSeverityExpectation
+		wantErr                string
+	}{
+		{
+			name: "happy path",
+			args: args{
+				cves: []PhotonCVE{
+					{
+						OSVersion: "1.0",
+						CveID:     "CVE-2019-10156",
+						Pkg:       "ansible",
+						CveScore:  5.4,
+						AffVer:    "all versions before 2.8.3-1.ph3 are vulnerable",
+						ResVer:    "2.8.3-1.ph3",
+					},
+				},
+			},
+			putAdvisory: db.PutAdvisoryExpectation{
+				Args: db.PutAdvisoryArgs{
+					TxAnything:      true,
+					Source:          "Photon OS 1.0",
+					PkgName:         "ansible",
+					VulnerabilityID: "CVE-2019-10156",
+					Advisory: types.Advisory{
+						FixedVersion: "2.8.3-1.ph3",
+					},
+				},
+			},
+			putVulnerabilityDetail: db.PutVulnerabilityDetailExpectation{
+				Args: db.PutVulnerabilityDetailArgs{
+					TxAnything:      true,
+					Source:          vulnerability.Photon,
+					VulnerabilityID: "CVE-2019-10156",
+					Vulnerability: types.VulnerabilityDetail{
+						CvssScore: 5.4,
+					},
+				},
+			},
+			putSeverity: db.PutSeverityExpectation{
+				Args: db.PutSeverityArgs{
+					TxAnything:      true,
+					VulnerabilityID: "CVE-2019-10156",
+					Severity:        types.SeverityUnknown,
+				},
+			},
+		},
+		{
+			name: "putAdvisory returns an error",
+			args: args{
+				cves: []PhotonCVE{
+					{
+						OSVersion: "1.0",
+						CveID:     "CVE-2019-10156",
+						Pkg:       "ansible",
+						CveScore:  5.4,
+						AffVer:    "all versions before 2.8.3-1.ph3 are vulnerable",
+						ResVer:    "2.8.3-1.ph3",
+					},
+				},
+			},
+			putAdvisory: db.PutAdvisoryExpectation{
+				Args: db.PutAdvisoryArgs{
+					TxAnything:      true,
+					Source:          "Photon OS 1.0",
+					PkgName:         "ansible",
+					VulnerabilityID: "CVE-2019-10156",
+					Advisory: types.Advisory{
+						FixedVersion: "2.8.3-1.ph3",
+					},
+				},
+				Returns: db.PutAdvisoryReturns{
+					Err: errors.New("error"),
+				},
+			},
+			wantErr: "failed to save Photon advisory",
+		},
+		{
+			name: "PutVulnerabilityDetail returns an error",
+			args: args{
+				cves: []PhotonCVE{
+					{
+						OSVersion: "1.0",
+						CveID:     "CVE-2019-10156",
+						Pkg:       "ansible",
+						CveScore:  5.4,
+						AffVer:    "all versions before 2.8.3-1.ph3 are vulnerable",
+						ResVer:    "2.8.3-1.ph3",
+					},
+				},
+			},
+			putAdvisory: db.PutAdvisoryExpectation{
+				Args: db.PutAdvisoryArgs{
+					TxAnything:      true,
+					Source:          "Photon OS 1.0",
+					PkgName:         "ansible",
+					VulnerabilityID: "CVE-2019-10156",
+					Advisory: types.Advisory{
+						FixedVersion: "2.8.3-1.ph3",
+					},
+				},
+			},
+			putVulnerabilityDetail: db.PutVulnerabilityDetailExpectation{
+				Args: db.PutVulnerabilityDetailArgs{
+					TxAnything:      true,
+					Source:          vulnerability.Photon,
+					VulnerabilityID: "CVE-2019-10156",
+					Vulnerability: types.VulnerabilityDetail{
+						CvssScore: 5.4,
+					},
+				},
+				Returns: db.PutVulnerabilityDetailReturns{
+					Err: errors.New("error"),
+				},
+			},
+			wantErr: "failed to save Photon vulnerability detail",
+		},
+		{
+			name: "happy path",
+			args: args{
+				cves: []PhotonCVE{
+					{
+						OSVersion: "1.0",
+						CveID:     "CVE-2019-10156",
+						Pkg:       "ansible",
+						CveScore:  5.4,
+						AffVer:    "all versions before 2.8.3-1.ph3 are vulnerable",
+						ResVer:    "2.8.3-1.ph3",
+					},
+				},
+			},
+			putAdvisory: db.PutAdvisoryExpectation{
+				Args: db.PutAdvisoryArgs{
+					TxAnything:      true,
+					Source:          "Photon OS 1.0",
+					PkgName:         "ansible",
+					VulnerabilityID: "CVE-2019-10156",
+					Advisory: types.Advisory{
+						FixedVersion: "2.8.3-1.ph3",
+					},
+				},
+			},
+			putVulnerabilityDetail: db.PutVulnerabilityDetailExpectation{
+				Args: db.PutVulnerabilityDetailArgs{
+					TxAnything:      true,
+					Source:          vulnerability.Photon,
+					VulnerabilityID: "CVE-2019-10156",
+					Vulnerability: types.VulnerabilityDetail{
+						CvssScore: 5.4,
+					},
+				},
+			},
+			putSeverity: db.PutSeverityExpectation{
+				Args: db.PutSeverityArgs{
+					TxAnything:      true,
+					VulnerabilityID: "CVE-2019-10156",
+					Severity:        types.SeverityUnknown,
+				},
+				Returns: db.PutSeverityReturns{
+					Err: errors.New("error"),
+				},
+			},
+			wantErr: "failed to save Photon vulnerability severity",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockDBConfig := new(db.MockOperation)
+			mockDBConfig.ApplyPutAdvisoryExpectation(tt.putAdvisory)
+			mockDBConfig.ApplyPutVulnerabilityDetailExpectation(tt.putVulnerabilityDetail)
+			mockDBConfig.ApplyPutSeverityExpectation(tt.putSeverity)
+
+			vs := VulnSrc{
+				dbc: mockDBConfig,
+			}
+			err := vs.commit(nil, tt.args.cves)
+
+			if tt.wantErr != "" {
+				require.NotNil(t, err, tt.name)
+				assert.Contains(t, err.Error(), tt.wantErr, tt.name)
+			} else {
+				assert.NoError(t, err, tt.name)
+			}
+		})
+	}
+}
