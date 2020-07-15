ab19b0a6f115864d62c5af02ae374588d07860f7
knqyf263 <knqyf263@gmail.com>
2020-07-15 13:28:28 +0300

    refactor(types): add deprecated comment

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -104,7 +104,7 @@ type Advisory struct {
 type Vulnerability struct {
 	Title          string         `json:",omitempty"`
 	Description    string         `json:",omitempty"`
-	Severity       string         `json:",omitempty"`
+	Severity       string         `json:",omitempty"` // Deprecated: Severity is only for backwards compatibility. Use VendorSeverity instead.
 	CweIDs         []string       `json:",omitempty"` // e.g. CWE-78, CWE-89
 	VendorSeverity VendorSeverity `json:",omitempty"`
 	VendorVectors  VendorVectors  `json:",omitempty"` // Deprecated: VendorVectors is only for backwards compatibility. Use CVSS instead.
