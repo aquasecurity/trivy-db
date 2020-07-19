feb5b53e63f89444c98b1360efc69c3474f42775
Rahul Yadav <rahulyadavsep92@gmail.com>
2020-07-19 20:01:57 +0530

    Ignore rejected vulnerability in NVD

--- pkg/vulnsrc/nvd/nvd.go
+++ pkg/vulnsrc/nvd/nvd.go
@@ -18,7 +18,8 @@ import (
 )
 
 const (
-	nvdDir = "nvd"
+	nvdDir              = "nvd"
+	rejectVulnerability = "** REJECT **"
 )
 
 type VulnSrc struct {
@@ -70,14 +71,23 @@ func (vs VulnSrc) commit(tx *bolt.Tx, items []Item) error {
 			references = append(references, ref.URL)
 		}
 
-		var description string
+		var (
+			isRejected  bool
+			description string
+		)
 		for _, d := range item.Cve.Description.DescriptionDataList {
 			if d.Value != "" {
 				description = d.Value
+				if strings.HasPrefix(description, rejectVulnerability) {
+					isRejected = true
+					break
+				}
 				break
 			}
 		}
-
+		if isRejected {
+			continue
+		}
 		var cweIDs []string
 		for _, data := range item.Cve.ProblemType.ProblemTypeData {
 			for _, desc := range data.Description {
