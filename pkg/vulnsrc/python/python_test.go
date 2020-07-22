f986eee6445597743763df2732d516ebb871c7e9
Rahul Yadav <rahulyadavsep92@gmail.com>
2020-07-22 20:21:56 +0530

    Updated tests

--- pkg/vulnsrc/python/python_test.go
+++ pkg/vulnsrc/python/python_test.go
@@ -2,10 +2,14 @@ package python
 
 import (
 	"os"
+	"reflect"
+	"sort"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 
+	"github.com/aquasecurity/trivy-db/pkg/types"
 	"github.com/aquasecurity/trivy-db/pkg/db"
 	"github.com/aquasecurity/trivy-db/pkg/vulnsrc/vulnerability"
 )
@@ -19,12 +23,36 @@ func TestVulnSrc_Update(t *testing.T) {
 		args struct {
 			dir string
 		}
+		expectedAdvisory []types.Advisory
 		expectErr bool
 	}{
 		{
 			name: "happy path",
 			args: args{
 				dir: "testdata/fixtures",
+			},
+			expectedAdvisory: []types.Advisory {
+				{
+					VulnerabilityID: "CVE-2018-9986",
+				},
+				{
+					VulnerabilityID: "CVE-2018-9987",
+				},
+				{
+					VulnerabilityID: "CVE-2018-9990",
+				},
+				{
+					VulnerabilityID: "CVE-2018-9999",
+				},
+				{
+					VulnerabilityID: "CVE-2019-18933",
+				},
+				{
+					VulnerabilityID: "CVE-2020-10935",
+				},
+				{
+					VulnerabilityID: "CVE-2020-9444",
+				},
 			},
 			expectErr: false,
 		},
@@ -37,7 +65,10 @@ func TestVulnSrc_Update(t *testing.T) {
 			assert.NoError(t, err, tc.name)
 			advisories, err := vulSec.dbc.GetAdvisories(vulnerability.PythonSafetyDB, "zulip")
 			assert.NoError(t, err, tc.name)
-			assert.Equal(t, 7, len(advisories), "expected 7 advisories for zulip found: %v", len(advisories))
+			sort.Slice(advisories[:], func(i, j int) bool {
+				return strings.Compare(advisories[i].VulnerabilityID, advisories[j].VulnerabilityID) <= 0
+			})
+			assert.True(t, reflect.DeepEqual(tc.expectedAdvisory,advisories), "expected %v for package zulip, got: %v",tc.expectedAdvisory, advisories)
 			os.RemoveAll("testdata/db")
 		})
 	}
