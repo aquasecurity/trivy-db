773a4d9abc07afba5f5aa1a487e1e8d28d5c60e3
knqyf263 <knqyf263@gmail.com>
2020-12-31 07:45:36 +0200

    fix(redhat-oval): replace redhat2 with redhat

--- pkg/vulnsrc/redhat-oval/redhat-oval.go
+++ pkg/vulnsrc/redhat-oval/redhat-oval.go
@@ -20,7 +20,7 @@ import (
 )
 
 var (
-	redhatDir = filepath.Join("oval", "redhat2")
+	redhatDir = filepath.Join("oval", "redhat")
 
 	// the same bucket name as Red Hat Security Data API
 	platformFormat = "Red Hat Enterprise Linux %s"
