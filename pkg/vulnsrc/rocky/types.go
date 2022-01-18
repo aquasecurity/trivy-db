734de3a1efcfbbd2a6799f58147a4260719fcd71
knqyf263 <knqyf263@gmail.com>
2022-01-18 22:25:47 +0200

    refactor: remove unused fields and tags

--- pkg/vulnsrc/rocky/types.go
+++ pkg/vulnsrc/rocky/types.go
@@ -2,36 +2,29 @@ package rocky
 
 // RLSA has detailed data of RLSA
 type RLSA struct {
-	ID          string      `xml:"id" json:"id,omitempty"`
-	Title       string      `xml:"title" json:"title,omitempty"`
-	Issued      Date        `xml:"issued" json:"issued,omitempty"`
-	Updated     Date        `xml:"updated" json:"updated,omitempty"`
-	Severity    string      `xml:"severity" json:"severity,omitempty"`
-	Description string      `xml:"description" json:"description,omitempty"`
-	Packages    []Package   `xml:"pkglist>collection>package" json:"packages,omitempty"`
-	References  []Reference `xml:"references>reference" json:"references,omitempty"`
+	ID          string      `json:"id,omitempty"`
+	Title       string      `json:"title,omitempty"`
+	Severity    string      `json:"severity,omitempty"`
+	Description string      `json:"description,omitempty"`
+	Packages    []Package   `json:"packages,omitempty"`
+	References  []Reference `json:"references,omitempty"`
 	CveIDs      []string    `json:"cveids,omitempty"`
-}
-
-// Date has time information
-type Date struct {
-	Date string `xml:"date,attr" json:"date,omitempty"`
 }
 
 // Reference has reference information
 type Reference struct {
-	Href  string `xml:"href,attr" json:"href,omitempty"`
-	ID    string `xml:"id,attr" json:"id,omitempty"`
-	Title string `xml:"title,attr" json:"title,omitempty"`
-	Type  string `xml:"type,attr" json:"type,omitempty"`
+	Href  string `json:"href,omitempty"`
+	ID    string `json:"id,omitempty"`
+	Title string `json:"title,omitempty"`
+	Type  string `json:"type,omitempty"`
 }
 
 // Package has affected package information
 type Package struct {
-	Name     string `xml:"name,attr" json:"name,omitempty"`
-	Epoch    string `xml:"epoch,attr" json:"epoch,omitempty"`
-	Version  string `xml:"version,attr" json:"version,omitempty"`
-	Release  string `xml:"release,attr" json:"release,omitempty"`
-	Arch     string `xml:"arch,attr" json:"arch,omitempty"`
-	Filename string `xml:"filename" json:"filename,omitempty"`
+	Name     string `json:"name,omitempty"`
+	Epoch    string `json:"epoch,omitempty"`
+	Version  string `json:"version,omitempty"`
+	Release  string `json:"release,omitempty"`
+	Arch     string `json:"arch,omitempty"`
+	Filename string `json:"filename,omitempty"`
 }
