8239b5dcf118553c617c943d6330f836ab21b5a6
DmitriyLewen <dmitriy.lewen@smartforce.io>
2022-02-03 13:14:55 +0600

    test(nvd): remove mock

--- pkg/vulnsrc/nvd/nvd_test.go
+++ pkg/vulnsrc/nvd/nvd_test.go
@@ -3,6 +3,7 @@ package nvd
 import (
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/aquasecurity/trivy-db/pkg/utils"
@@ -27,7 +28,7 @@ func TestVulnSrc_Update(t *testing.T) {
 	}{
 		{
 			name:  "happy path",
-			dir:   "./testdata",
+			dir:   filepath.Join("testdata", "happy"),
 			cveID: "CVE-2020-0001",
 			want: types.VulnerabilityDetail{
 				Description:      "In getProcessRecordLocked of ActivityManagerService.java isolated apps are not handled correctly. This could lead to local escalation of privilege with no additional execution privileges needed. User interaction is not needed for exploitation. Product: Android Versions: Android-8.0, Android-8.1, Android-9, and Android-10 Android ID: A-140055304",
@@ -44,9 +45,32 @@ func TestVulnSrc_Update(t *testing.T) {
 			},
 		},
 		{
-			name:    "sad path",
-			dir:     "./sad",
+			name:  "happy path (**REJECT** in description)",
+			dir:   filepath.Join("testdata", "reject in description"),
+			cveID: "CVE-2020-0001",
+			want: types.VulnerabilityDetail{
+				Description:      "** REJECT ** test description",
+				CvssScore:        7.2,
+				CvssVector:       "AV:L/AC:L/Au:N/C:C/I:C/A:C",
+				CvssScoreV3:      7.8,
+				CvssVectorV3:     "CVSS:3.1/AV:L/AC:L/PR:L/UI:N/S:U/C:H/I:H/A:H",
+				Severity:         types.SeverityHigh,
+				SeverityV3:       types.SeverityHigh,
+				CweIDs:           []string{"CWE-269"},
+				References:       []string{"https://source.android.com/security/bulletin/2020-01-01"},
+				LastModifiedDate: utils.MustTimeParse("2020-01-01T01:01:00Z"),
+				PublishedDate:    utils.MustTimeParse("2001-01-01T01:01:00Z"),
+			},
+		},
+		{
+			name:    "sad path (dir doesn't exist)",
+			dir:     filepath.Join("testdata", "badpathdoesnotexist"),
 			wantErr: "no such file or directory",
+		},
+		{
+			name:    "sad path (failed to decode)",
+			dir:     filepath.Join("testdata", "sad"),
+			wantErr: "failed to decode NVD JSON",
 		},
 	}
 
