4eff4d40654ff114ab6c56070a89433f76cd20b8
AMF <work@afdesk.com>
2022-08-30 21:32:15 +0600

    add GetUniqueIDsFromReferences

---
+++ pkg/utils/ids.go
@@ -0,0 +1,55 @@
+package utils
+
+import (
+	"net/url"
+	"strings"
+
+	strutil "github.com/aquasecurity/trivy-db/pkg/utils/strings"
+)
+
+const (
+	nvd     = "nvd.nist.gov"
+	rustsec = "rustsec.org"
+	ghsa    = "github.com"
+)
+
+func GetUniqueIDsFromReferences(refs []string, knownIDs []string) []string {
+	condidates := map[string]interface{}{}
+	for _, ref := range refs {
+		u, err := url.Parse(ref)
+		if err != nil {
+			continue
+		}
+		id := ""
+		switch u.Host {
+		// https://rustsec.org/advisories/RUSTSEC-2021-0120.html"
+		case rustsec:
+			if strings.HasPrefix(u.Path, "/advisories/") {
+				id = strings.TrimSuffix(strings.TrimPrefix(u.Path, "/advisories/"), ".html")
+			}
+			break
+			// https://nvd.nist.gov/vuln/detail/CVE-2021-45708
+		case nvd:
+			if strings.HasPrefix(u.Path, "/vuln/detail/") {
+				id = strings.TrimPrefix(u.Path, "/vuln/detail/")
+			}
+			break
+			// https://github.com/advisories/GHSA-4qrp-27r3-66fj
+		case ghsa:
+			if strings.HasPrefix(u.Path, "/advisories/") {
+				id = strings.TrimPrefix(u.Path, "/advisories/")
+			}
+			break
+		default:
+			continue
+		}
+		if id != "" && !strutil.InSlice(id, knownIDs) {
+			condidates[id] = struct{}{}
+		}
+	}
+	ids := []string{}
+	for k := range condidates {
+		ids = append(ids, k)
+	}
+	return ids
+}
---
+++ pkg/utils/ids_test.go
@@ -0,0 +1,57 @@
+package utils
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetUniqueIDsFromReferences(t *testing.T) {
+	tests := []struct {
+		title      string
+		known      []string
+		references []string
+		ids        []string
+	}{
+		{
+			title: "happy path",
+			known: []string{},
+			references: []string{
+				"https://github.com/Sylius/Sylius/security/advisories/GHSA-4qrp-27r3-66fj",
+				"https://github.com/Sylius/Sylius/releases/tag/v1.10.11",
+				"https://github.com/Sylius/Sylius/releases/tag/v1.11.2",
+				"https://github.com/Sylius/Sylius/releases/tag/v1.9.10",
+				"https://nvd.nist.gov/vuln/detail/CVE-2022-24749",
+				"https://github.com/advisories/GHSA-4qrp-27r3-66fj",
+			},
+			ids: []string{
+				"CVE-2022-24749",
+				"GHSA-4qrp-27r3-66fj",
+			},
+		},
+		{
+			title: "there is a known ID",
+			known: []string{
+				"GHSA-4qrp-27r3-66fj",
+			},
+			references: []string{
+				"https://github.com/Sylius/Sylius/security/advisories/GHSA-4qrp-27r3-66fj",
+				"https://github.com/Sylius/Sylius/releases/tag/v1.10.11",
+				"https://github.com/Sylius/Sylius/releases/tag/v1.11.2",
+				"https://github.com/Sylius/Sylius/releases/tag/v1.9.10",
+				"https://nvd.nist.gov/vuln/detail/CVE-2022-24749",
+				"https://github.com/advisories/GHSA-4qrp-27r3-66fj",
+			},
+			ids: []string{
+				"CVE-2022-24749",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.title, func(t *testing.T) {
+			got := GetUniqueIDsFromReferences(tt.references, tt.known)
+			assert.Equal(t, tt.ids, got)
+		})
+	}
+}
