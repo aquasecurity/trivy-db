9dd4c7776a5249532b5d427a3d62f2d31f1a4706
DmitriyLewen <91113035+DmitriyLewen@users.noreply.github.com>
2022-09-04 15:07:34 +0600

    feat(glad): add conan

--- pkg/vulnsrc/glad/glad_test.go
+++ pkg/vulnsrc/glad/glad_test.go
@@ -1,13 +1,13 @@
 package glad_test
 
 import (
-	"github.com/aquasecurity/trivy-db/pkg/vulnsrctest"
 	"path/filepath"
 	"testing"
 
 	"github.com/aquasecurity/trivy-db/pkg/types"
 	"github.com/aquasecurity/trivy-db/pkg/vulnsrc/glad"
 	"github.com/aquasecurity/trivy-db/pkg/vulnsrc/vulnerability"
+	"github.com/aquasecurity/trivy-db/pkg/vulnsrctest"
 )
 
 func TestVulnSrc_Update(t *testing.T) {
@@ -38,6 +38,14 @@ func TestVulnSrc_Update(t *testing.T) {
 					},
 				},
 				{
+					Key: []string{"data-source", "conan::GitLab Advisory Database Community"},
+					Value: types.DataSource{
+						ID:   vulnerability.GLAD,
+						Name: "GitLab Advisory Database Community",
+						URL:  "https://gitlab.com/gitlab-org/advisories-community",
+					},
+				},
+				{
 					Key: []string{"advisory-detail", "CVE-2016-1905", "go::GitLab Advisory Database Community", "k8s.io/kubernetes"},
 					Value: types.Advisory{
 						PatchedVersions:    []string{"v1.2.0"},
@@ -49,6 +57,12 @@ func TestVulnSrc_Update(t *testing.T) {
 					Value: types.Advisory{
 						PatchedVersions:    []string{"1.5.10.RELEASE"},
 						VulnerableVersions: []string{"(,1.5.10)"},
+					},
+				},
+				{
+					Key: []string{"advisory-detail", "CVE-2020-13574", "conan::GitLab Advisory Database Community", "gsoap"},
+					Value: types.Advisory{
+						VulnerableVersions: []string{"=2.8.107"},
 					},
 				},
 				{
@@ -67,6 +81,15 @@ func TestVulnSrc_Update(t *testing.T) {
 						Title:       "Symlink privilege escalation attack via Spring Boot launch script",
 						Description: "Spring Boot supports an embedded launch script that can be used to easily run the application as a systemd or init.d linux service. The script included with Spring Boot is susceptible to a symlink attack which allows the `run_user` to overwrite and take ownership of any file on the same system. In order to instigate the attack, the application must be installed as a service and the `run_user` requires shell access to the server.",
 						References:  []string{"https://pivotal.io/security/cve-2018-1196"},
+					},
+				},
+				{
+					Key: []string{"vulnerability-detail", "CVE-2020-13574", "glad"},
+					Value: types.VulnerabilityDetail{
+						ID:          "CVE-2020-13574",
+						Title:       "NULL Pointer Dereference",
+						Description: "A denial-of-service vulnerability exists in the WS-Security plugin functionality of Genivia gSOAP. A specially crafted SOAP request can lead to denial of service. An attacker can send an HTTP request to trigger this vulnerability.",
+						References:  []string{"https://nvd.nist.gov/vuln/detail/CVE-2020-13574"},
 					},
 				},
 			},
