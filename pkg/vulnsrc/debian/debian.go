f5ad7d592115bef4341cf938eae3eb175e91cdcd
DmitriyLewen <dmitriy.lewen@smartforce.io>
2022-11-21 16:10:06 +0600

    add ignored to skipStatuses

--- pkg/vulnsrc/debian/debian.go
+++ pkg/vulnsrc/debian/debian.go
@@ -42,7 +42,7 @@ const (
 var (
 	// NOTE: "removed" should not be marked as "not-affected".
 	// ref. https://security-team.debian.org/security_tracker.html#removed-packages
-	skipStatuses = []string{"not-affected", "undetermined"}
+	skipStatuses = []string{"not-affected", "undetermined", "ignored"}
 
 	source = types.DataSource{
 		ID:   vulnerability.Debian,
