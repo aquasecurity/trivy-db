34511057110e63c22990cf60340b4b48b2b19dd2
knqyf263 <knqyf263@gmail.com>
2023-06-22 18:22:11 +0300

    test: add Get tests

--- pkg/vulnsrc/rocky/rocky_test.go
+++ pkg/vulnsrc/rocky/rocky_test.go
@@ -1,12 +1,18 @@
-package rocky
+package rocky_test
 
 import (
-	"github.com/aquasecurity/trivy-db/pkg/vulnsrctest"
 	"path/filepath"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/aquasecurity/trivy-db/pkg/db"
+	"github.com/aquasecurity/trivy-db/pkg/dbtest"
 	"github.com/aquasecurity/trivy-db/pkg/types"
+	"github.com/aquasecurity/trivy-db/pkg/vulnsrc/rocky"
 	"github.com/aquasecurity/trivy-db/pkg/vulnsrc/vulnerability"
+	"github.com/aquasecurity/trivy-db/pkg/vulnsrctest"
 )
 
 func TestVulnSrc_Update(t *testing.T) {
@@ -21,7 +27,10 @@ func TestVulnSrc_Update(t *testing.T) {
 			dir:  filepath.Join("testdata", "happy"),
 			wantValues: []vulnsrctest.WantValues{
 				{
-					Key: []string{"data-source", "rocky 8"},
+					Key: []string{
+						"data-source",
+						"rocky 8",
+					},
 					Value: types.DataSource{
 						ID:   vulnerability.Rocky,
 						Name: "Rocky Linux updateinfo",
@@ -29,53 +38,55 @@ func TestVulnSrc_Update(t *testing.T) {
 					},
 				},
 				{
-					Key: []string{"advisory-detail", "CVE-2021-25215", "rocky 8", "bind-export-libs"},
-					Value: types.Advisory{
+					Key: []string{
+						"advisory-detail",
+						"CVE-2021-25215",
+						"rocky 8",
+						"bind-export-libs",
+					},
+					Value: rocky.Advisory{
 						FixedVersion: "32:9.11.26-4.el8_4",
-						Entries: []types.Entry{
+						Entries: []types.Advisory{
 							{
 								FixedVersion: "32:9.11.26-4.el8_4",
-								Arch:         "aarch64",
-								VendorID:     "RLSA-2021:1989",
-							},
-							{
-								FixedVersion: "32:9.11.26-4.el8_4",
-								Arch:         "i686",
-								VendorID:     "RLSA-2021:1989",
-							},
-							{
-								FixedVersion: "32:9.11.26-4.el8_4",
-								Arch:         "x86_64",
-								VendorID:     "RLSA-2021:1989",
+								Arches: []string{
+									"aarch64",
+									"i686",
+									"x86_64",
+								},
+								VendorIDs: []string{"RLSA-2021:1989"},
 							},
 						},
 					},
 				},
 				{
-					Key: []string{"advisory-detail", "CVE-2021-25215", "rocky 8", "bind-export-devel"},
-					Value: types.Advisory{
+					Key: []string{
+						"advisory-detail",
+						"CVE-2021-25215",
+						"rocky 8",
+						"bind-export-devel",
+					},
+					Value: rocky.Advisory{
 						FixedVersion: "32:9.11.26-4.el8_4",
-						Entries: []types.Entry{
+						Entries: []types.Advisory{
 							{
 								FixedVersion: "32:9.11.26-4.el8_4",
-								Arch:         "aarch64",
-								VendorID:     "RLSA-2021:1989",
-							},
-							{
-								FixedVersion: "32:9.11.26-4.el8_4",
-								Arch:         "x86_64",
-								VendorID:     "RLSA-2021:1989",
-							},
-							{
-								FixedVersion: "32:9.11.26-4.el8_4",
-								Arch:         "i686",
-								VendorID:     "RLSA-2021:1989",
+								Arches: []string{
+									"aarch64",
+									"i686",
+									"x86_64",
+								},
+								VendorIDs: []string{"RLSA-2021:1989"},
 							},
 						},
 					},
 				},
 				{
-					Key: []string{"vulnerability-detail", "CVE-2021-25215", string(vulnerability.Rocky)},
+					Key: []string{
+						"vulnerability-detail",
+						"CVE-2021-25215",
+						string(vulnerability.Rocky),
+					},
 					Value: types.VulnerabilityDetail{
 						Severity: types.SeverityHigh,
 						References: []string{
@@ -86,7 +97,10 @@ func TestVulnSrc_Update(t *testing.T) {
 					},
 				},
 				{
-					Key:   []string{"vulnerability-id", "CVE-2021-25215"},
+					Key: []string{
+						"vulnerability-id",
+						"CVE-2021-25215",
+					},
 					Value: map[string]interface{}{},
 				},
 			},
@@ -104,12 +118,91 @@ func TestVulnSrc_Update(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			vs := NewVulnSrc()
+			vs := rocky.NewVulnSrc()
 			vulnsrctest.TestUpdate(t, vs, vulnsrctest.TestUpdateArgs{
 				Dir:        tt.dir,
 				WantValues: tt.wantValues,
 				WantErr:    tt.wantErr,
 			})
+		})
+	}
+}
+
+func TestRocky_Get(t *testing.T) {
+	type args struct {
+		release string
+		pkgName string
+		arch    string
+	}
+	tests := []struct {
+		name     string
+		args     args
+		fixtures []string
+		want     []types.Advisory
+		wantErr  require.ErrorAssertionFunc
+	}{
+		{
+			name:     "the same fixed version",
+			fixtures: []string{"testdata/fixtures/happy.yaml"},
+			args: args{
+				release: "9",
+				pkgName: "bind",
+				arch:    "x86_64",
+			},
+			want: []types.Advisory{
+				{
+					VulnerabilityID: "CVE-2022-0396",
+					FixedVersion:    "32:9.16.23-0.9.el8.1",
+					Arches: []string{
+						"aarch64",
+						"x86_64",
+					},
+					VendorIDs: []string{"RLSA-2022:7643"},
+				},
+			},
+			wantErr: require.NoError,
+		},
+		{
+			name:     "different fixed versions for different arches",
+			fixtures: []string{"testdata/fixtures/happy.yaml"},
+			args: args{
+				release: "9",
+				pkgName: "rsyslog",
+				arch:    "aarch64",
+			},
+			want: []types.Advisory{
+				{
+					VulnerabilityID: "CVE-2022-24903",
+					FixedVersion:    "8.2102.0-7.el8_6.2",
+					Arches: []string{
+						"aarch64",
+					},
+					VendorIDs: []string{"RLSA-2022:4799"},
+				},
+			},
+			wantErr: require.NoError,
+		},
+		{
+			name:     "broken JSON",
+			fixtures: []string{"testdata/fixtures/broken.yaml"},
+			args: args{
+				release: "9",
+				pkgName: "bind",
+				arch:    "aarch64",
+			},
+			wantErr: require.Error,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_ = dbtest.InitDB(t, tt.fixtures)
+			defer db.Close()
+
+			vs := rocky.NewVulnSrc()
+			got, err := vs.Get(tt.args.release, tt.args.pkgName, tt.args.arch)
+
+			tt.wantErr(t, err)
+			assert.Equal(t, tt.want, got)
 		})
 	}
 }
