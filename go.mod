bcd98c4fdb68bc232adb41a1f2564ff41a2895bc
Teppei Fukuda <knqyf263@gmail.com>
2023-08-28 11:17:45 +0300

    feat: use the GHSA repository (#345)
    Co-authored-by: DmitriyLewen <dmitriy.lewen@smartforce.io>

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/aquasecurity/bolt-fixtures v0.0.0-20200903104109-d34e7f983986
 	github.com/briandowns/spinner v1.23.0
 	github.com/fatih/color v1.10.0 // indirect
+	github.com/goark/go-cvss v1.6.6
 	github.com/hashicorp/go-version v1.6.0
 	github.com/knqyf263/go-deb-version v0.0.0-20190517075300-09fca494f03d
 	github.com/knqyf263/go-rpm-version v0.0.0-20170716094938-74609b86c936
@@ -24,6 +25,7 @@ require (
 require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/goark/errs v1.1.0 // indirect
 	github.com/goccy/go-yaml v1.8.1 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/mattn/go-colorable v0.1.8 // indirect
--- pkg/vulnsrc/osv/osv.go
+++ pkg/vulnsrc/osv/osv.go
@@ -4,12 +4,13 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"log"
 	"path/filepath"
 	"strings"
 	"time"
 
+	"github.com/goark/go-cvss/v3/metric"
 	bolt "go.etcd.io/bbolt"
+	"golang.org/x/exp/maps"
 	"golang.org/x/xerrors"
 
 	"github.com/aquasecurity/trivy-db/pkg/db"
@@ -19,98 +20,87 @@ import (
 	"github.com/aquasecurity/trivy-db/pkg/vulnsrc/vulnerability"
 )
 
-const (
-	osvDir     = "osv"
-	dataSource = "Open Source Vulnerability"
-	sourceID   = vulnerability.OSV
-)
+type Advisory struct {
+	Ecosystem       types.Ecosystem
+	PkgName         string
+	VulnerabilityID string
+	Aliases         []string
 
-var ecosystems = []ecosystem{
-	{
-		dir:  "python",
-		name: vulnerability.Pip,
-		dataSource: types.DataSource{
-			ID:   sourceID,
-			Name: "Python Packaging Advisory Database",
-			URL:  "https://github.com/pypa/advisory-db",
-		},
-	},
-	// Cargo ecosystem advisories in OSV were disabled,
-	// because GitHub Advisory Database contains almost all information.
-	/*
-		{
-			dir:  "rust",
-			name: vulnerability.Cargo,
-			dataSource: types.DataSource{
-				ID:   sourceID,
-				Name: "RustSec Advisory Database",
-				URL:  "https://github.com/RustSec/advisory-db",
-			},
-		},
-	*/
+	// Advisory detail
+	VulnerableVersions []string
+	PatchedVersions    []string
 
-	// Go ecosystem advisories in OSV were disabled,
-	// because GitHub Advisory Database contains almost all information.
-	//{dir: "go", pkgType: vulnerability.Go, sourceID: vulnerability.OSVGo},
+	// Vulnerability detail
+	Severity     types.Severity
+	Title        string
+	Description  string
+	References   []string
+	CVSSScoreV3  float64
+	CVSSVectorV3 string
 }
 
-type ecosystem struct {
-	dir        string
-	name       types.Ecosystem
-	dataSource types.DataSource
+type OSV struct {
+	dir         string
+	dbc         db.Operation
+	sourceID    types.SourceID
+	dataSources map[types.Ecosystem]types.DataSource
+	transformer Transformer
 }
 
-type VulnSrc struct {
-	dbc db.Operation
+type Transformer interface {
+	TransformAdvisories([]Advisory, Entry) ([]Advisory, error)
 }
 
-func NewVulnSrc() VulnSrc {
-	return VulnSrc{
-		dbc: db.Config{},
+type defaultTransformer struct{}
+
+func (t *defaultTransformer) TransformAdvisories(advs []Advisory, _ Entry) ([]Advisory, error) {
+	return advs, nil
+}
+
+func New(dir string, sourceID types.SourceID, dataSources map[types.Ecosystem]types.DataSource, transformer Transformer) OSV {
+	if transformer == nil {
+		transformer = &defaultTransformer{}
+	}
+	return OSV{
+		dir:         dir,
+		dbc:         db.Config{},
+		sourceID:    sourceID,
+		dataSources: dataSources,
+		transformer: transformer,
 	}
 }
 
-func (vs VulnSrc) Name() types.SourceID {
-	return sourceID
+func (o OSV) Name() types.SourceID {
+	return o.sourceID
 }
 
-func (vs VulnSrc) Update(dir string) error {
-	for _, eco := range ecosystems {
-		log.Printf("    Updating Open Source Vulnerability %s", eco.name)
-		rootDir := filepath.Join(dir, "vuln-list", osvDir, eco.dir)
+func (o OSV) Update(root string) error {
+	rootDir := filepath.Join(root, o.dir)
 
-		var entries []Entry
-		err := utils.FileWalk(rootDir, func(r io.Reader, path string) error {
-			var entry Entry
-			if err := json.NewDecoder(r).Decode(&entry); err != nil {
-				return xerrors.Errorf("JSON decode error (%s): %w", path, err)
-			}
-
-			// GHSA-IDs are already stored via ghsa package.
-			// Skip them to avoid duplication.
-			if strings.HasPrefix(entry.ID, "GHSA") {
-				return nil
-			}
-
-			entries = append(entries, entry)
-			return nil
-		})
-		if err != nil {
-			return xerrors.Errorf("walk error: %w", err)
+	var entries []Entry
+	err := utils.FileWalk(rootDir, func(r io.Reader, path string) error {
+		var entry Entry
+		if err := json.NewDecoder(r).Decode(&entry); err != nil {
+			return xerrors.Errorf("JSON decode error (%s): %w", path, err)
 		}
+		entries = append(entries, entry)
+		return nil
+	})
+	if err != nil {
+		return xerrors.Errorf("walk error: %w", err)
+	}
 
-		if err = vs.save(eco, entries); err != nil {
-			return xerrors.Errorf("save error: %w", err)
-		}
+	if err = o.save(entries); err != nil {
+		return xerrors.Errorf("save error: %w", err)
 	}
 
 	return nil
 }
 
-func (vs VulnSrc) save(eco ecosystem, entries []Entry) error {
-	err := vs.dbc.BatchUpdate(func(tx *bolt.Tx) error {
+func (o OSV) save(entries []Entry) error {
+	err := o.dbc.BatchUpdate(func(tx *bolt.Tx) error {
 		for _, entry := range entries {
-			if err := vs.commit(tx, eco, entry); err != nil {
+			if err := o.commit(tx, entry); err != nil {
 				return err
 			}
 		}
@@ -122,32 +112,26 @@ func (vs VulnSrc) save(eco ecosystem, entries []Entry) error {
 	return nil
 }
 
-func (vs VulnSrc) commit(tx *bolt.Tx, eco ecosystem, entry Entry) error {
-
+func (o OSV) commit(tx *bolt.Tx, entry Entry) error {
 	if entry.Withdrawn != nil && entry.Withdrawn.Before(time.Now()) {
 		return nil
 	}
 
-	bktName := bucket.Name(string(eco.name), dataSource)
-
-	if err := vs.dbc.PutDataSource(tx, bktName, eco.dataSource); err != nil {
-		return xerrors.Errorf("failed to put data source: %w", err)
-	}
-
-	// Aliases contain CVE-IDs
-	vulnIDs := filterCveIDs(entry.Aliases)
-	if len(vulnIDs) == 0 {
-		// e.g. PYSEC-2021-335
-		vulnIDs = []string{entry.ID}
-	}
+	// Group IDs into primary vulnerability IDs and aliases.
+	vulnIDs, aliases := groupVulnIDs(entry.ID, entry.Aliases)
 
 	var references []string
 	for _, ref := range entry.References {
 		references = append(references, ref.URL)
 	}
 
+	uniqAdvisories := map[string]Advisory{}
 	for _, affected := range entry.Affected {
-		pkgName := vulnerability.NormalizePkgName(eco.name, affected.Package.Name)
+		ecosystem := convertEcosystem(affected.Package.Ecosystem)
+		if ecosystem == vulnerability.Unknown {
+			continue
+		}
+		pkgName := vulnerability.NormalizePkgName(ecosystem, affected.Package.Name)
 		var patchedVersions, vulnerableVersions []string
 		for _, affects := range affected.Ranges {
 			if affects.Type == RangeTypeGit {
@@ -180,42 +164,143 @@ func (vs VulnSrc) commit(tx *bolt.Tx, eco ecosystem, entry Entry) error {
 			}
 		}
 
-		advisory := types.Advisory{
-			VulnerableVersions: vulnerableVersions,
-			PatchedVersions:    patchedVersions,
+		var (
+			cvssVectorV3 string
+			cvssScoreV3  float64
+		)
+		for _, s := range entry.Severities {
+			// cf. https://ossf.github.io/osv-schema/#severitytype-field
+			if s.Type == "CVSS_V3" {
+				// some GHSA vectors have `/` suffix
+				// e.g. https://github.com/github/advisory-database/blob/2d3bc73d2117893b217233aeb95b9236c7b93761/advisories/github-reviewed/2019/05/GHSA-j59f-6m4q-62h6/GHSA-j59f-6m4q-62h6.json#L14
+				// trim this suffix to avoid errors
+				cvssVectorV3 = strings.TrimSuffix(s.Score, "/")
+				metrics, err := metric.NewTemporal().Decode(cvssVectorV3)
+				if err != nil {
+					return xerrors.Errorf("failed to decode CVSSv3 vector: %w", err)
+				}
+				cvssScoreV3 = metrics.Score()
+			}
 		}
 
+		key := fmt.Sprintf("%s/%s", ecosystem, pkgName)
 		for _, vulnID := range vulnIDs {
-			if err := vs.dbc.PutAdvisoryDetail(tx, vulnID, pkgName, []string{bktName}, advisory); err != nil {
-				return xerrors.Errorf("failed to save OSV advisory: %w", err)
+			if adv, ok := uniqAdvisories[key]; ok {
+				// The same package could be repeated with different version ranges.
+				// cf. https://github.com/github/advisory-database/blob/0996f81ca6f1b65ba25f8e71fba263cb1e54ced5/advisories/github-reviewed/2019/12/GHSA-wjx8-cgrm-hh8p/GHSA-wjx8-cgrm-hh8p.json
+				adv.VulnerableVersions = append(adv.VulnerableVersions, vulnerableVersions...)
+				adv.PatchedVersions = append(adv.PatchedVersions, patchedVersions...)
+				uniqAdvisories[key] = adv
+			} else {
+				uniqAdvisories[key] = Advisory{
+					Ecosystem:          ecosystem,
+					PkgName:            pkgName,
+					VulnerabilityID:    vulnID,
+					Aliases:            aliases,
+					VulnerableVersions: vulnerableVersions,
+					PatchedVersions:    patchedVersions,
+					Title:              entry.Summary,
+					Description:        entry.Details,
+					References:         references,
+					CVSSVectorV3:       cvssVectorV3,
+					CVSSScoreV3:        cvssScoreV3,
+				}
 			}
 		}
 	}
 
-	for _, vulnID := range vulnIDs {
+	// Transform advisories
+	advisories, err := o.transformer.TransformAdvisories(maps.Values(uniqAdvisories), entry)
+	if err != nil {
+		return xerrors.Errorf("failed to transform advisories: %w", err)
+	}
+
+	for _, adv := range advisories {
+		dataSource, ok := o.dataSources[adv.Ecosystem]
+		if !ok {
+			continue
+		}
+		bktName := bucket.Name(string(adv.Ecosystem), dataSource.Name)
+
+		if err = o.dbc.PutDataSource(tx, bktName, dataSource); err != nil {
+			return xerrors.Errorf("failed to put data source: %w", err)
+		}
+
+		// Store advisories
+		advisory := types.Advisory{
+			VendorIDs:          adv.Aliases,
+			VulnerableVersions: adv.VulnerableVersions,
+			PatchedVersions:    adv.PatchedVersions,
+		}
+		if err = o.dbc.PutAdvisoryDetail(tx, adv.VulnerabilityID, adv.PkgName, []string{bktName}, advisory); err != nil {
+			return xerrors.Errorf("failed to save OSV advisory: %w", err)
+		}
+
+		// Store vulnerability details
 		vuln := types.VulnerabilityDetail{
-			Title:       entry.Summary,
-			Description: entry.Details,
-			References:  references,
+			Severity:     adv.Severity,
+			References:   adv.References,
+			Title:        adv.Title,
+			Description:  adv.Description,
+			CvssScoreV3:  adv.CVSSScoreV3,
+			CvssVectorV3: adv.CVSSVectorV3,
 		}
 
-		if err := vs.dbc.PutVulnerabilityDetail(tx, vulnID, sourceID, vuln); err != nil {
-			return xerrors.Errorf("failed to put vulnerability detail (%s): %w", vulnID, err)
+		if err = o.dbc.PutVulnerabilityDetail(tx, adv.VulnerabilityID, o.sourceID, vuln); err != nil {
+			return xerrors.Errorf("failed to put vulnerability detail (%s): %w", adv.VulnerabilityID, err)
 		}
 
-		if err := vs.dbc.PutVulnerabilityID(tx, vulnID); err != nil {
-			return xerrors.Errorf("failed to put vulnerability id (%s): %w", vulnID, err)
+		if err = o.dbc.PutVulnerabilityID(tx, adv.VulnerabilityID); err != nil {
+			return xerrors.Errorf("failed to put vulnerability id (%s): %w", adv.VulnerabilityID, err)
 		}
 	}
 	return nil
 }
 
-func filterCveIDs(aliases []string) []string {
-	var cveIDs []string
-	for _, a := range aliases {
+func groupVulnIDs(id string, aliases []string) ([]string, []string) {
+	var cveIDs, nonCVEIDs []string
+	for _, a := range append(aliases, id) {
 		if strings.HasPrefix(a, "CVE-") {
 			cveIDs = append(cveIDs, a)
+		} else {
+			nonCVEIDs = append(nonCVEIDs, a)
 		}
 	}
-	return cveIDs
+	if len(cveIDs) == 0 {
+		// Use the original vulnerability ID
+		// e.g. PYSEC-2021-335 and GHSA-wjx8-cgrm-hh8p
+		return []string{id}, aliases
+	}
+	return cveIDs, nonCVEIDs
+}
+
+func convertEcosystem(eco Ecosystem) types.Ecosystem {
+	// cf. https://ossf.github.io/osv-schema/#affectedpackage-field
+	switch eco {
+	case "Go":
+		return vulnerability.Go
+	case "npm":
+		return vulnerability.Npm
+	case "PyPI":
+		return vulnerability.Pip
+	case "RubyGems":
+		return vulnerability.RubyGems
+	case "crates.io":
+		return vulnerability.Cargo
+	case "Packagist":
+		return vulnerability.Composer
+	case "Maven":
+		return vulnerability.Maven
+	case "NuGet":
+		return vulnerability.NuGet
+	case "Hex":
+		return vulnerability.Erlang
+	case "Pub":
+		return vulnerability.Pub
+	case "SwiftURL", "purl-type:swift":
+		// GHSA still uses "purl-type:swift" for Swift advisories.
+		// cf. https://github.com/github/advisory-database/blob/db1cdfb553e48f18aa27d7e929d200563451391a/advisories/github-reviewed/2023/07/GHSA-jq43-q8mx-r7mq/GHSA-jq43-q8mx-r7mq.json#L20
+		return vulnerability.Swift
+	}
+	return vulnerability.Unknown
 }
