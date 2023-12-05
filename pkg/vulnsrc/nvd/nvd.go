b7e04723a2a587899f6390288d4542a7219feae7
DmitriyLewen <dmitriy.lewen@smartforce.io>
2023-12-05 11:56:31 +0600

    refactor: use `api` dir

--- pkg/vulnsrc/nvd/nvd.go
+++ pkg/vulnsrc/nvd/nvd.go
@@ -20,7 +20,8 @@ import (
 
 const (
 	vulnListDir = "vuln-list-nvd"
-	feedDir     = "feed"
+	apiDir      = "api"
+	primaryType = "Primary" // NVD has 2 type enums: `Primary` and `Secondary`
 )
 
 type VulnSrc struct {
@@ -38,71 +39,91 @@ func (vs VulnSrc) Name() types.SourceID {
 }
 
 func (vs VulnSrc) Update(dir string) error {
-	rootDir := filepath.Join(dir, vulnListDir, feedDir)
+	rootDir := filepath.Join(dir, vulnListDir, apiDir)
 
-	var items []Item
+	var cves []Cve
 	buffer := &bytes.Buffer{}
 	err := utils.FileWalk(rootDir, func(r io.Reader, _ string) error {
-		item := Item{}
+		cve := Cve{}
 		if _, err := buffer.ReadFrom(r); err != nil {
 			return xerrors.Errorf("failed to read file: %w", err)
 		}
-		if err := json.Unmarshal(buffer.Bytes(), &item); err != nil {
+		if err := json.Unmarshal(buffer.Bytes(), &cve); err != nil {
 			return xerrors.Errorf("failed to decode NVD JSON: %w", err)
 		}
 		buffer.Reset()
-		items = append(items, item)
+		cves = append(cves, cve)
 		return nil
 	})
 	if err != nil {
 		return xerrors.Errorf("error in NVD walk: %w", err)
 	}
 
-	if err = vs.save(items); err != nil {
+	if err = vs.save(cves); err != nil {
 		return xerrors.Errorf("error in NVD save: %w", err)
 	}
 
 	return nil
 }
 
-func (vs VulnSrc) commit(tx *bolt.Tx, items []Item) error {
-	for _, item := range items {
-		cveID := item.Cve.Meta.ID
-		severity, _ := types.NewSeverity(item.Impact.BaseMetricV2.Severity)
-		severityV3, _ := types.NewSeverity(item.Impact.BaseMetricV3.CvssV3.BaseSeverity)
+func (vs VulnSrc) commit(tx *bolt.Tx, cves []Cve) error {
+	for _, cve := range cves {
+		cveID := cve.ID
+
+		var cvssScore, cvssScoreV3 float64
+		var cvssVector, CvssVectorV3 string
+		var severity, severityV3 types.Severity
+		for _, metricV2 := range cve.Metrics.CvssMetricV2 {
+			if metricV2.CvssData.BaseScore != 0 && metricV2.CvssData.VectorString != "" && metricV2.BaseSeverity != "" {
+				cvssScore = metricV2.CvssData.BaseScore
+				cvssVector = metricV2.CvssData.VectorString
+				severity, _ = types.NewSeverity(metricV2.BaseSeverity)
+				if metricV2.Type == primaryType {
+					break
+				}
+			}
+		}
+		for _, metricV31 := range cve.Metrics.CvssMetricV31 {
+			if metricV31.CvssData.BaseScore != 0 && metricV31.CvssData.VectorString != "" && metricV31.CvssData.BaseSeverity != "" {
+				cvssScoreV3 = metricV31.CvssData.BaseScore
+				CvssVectorV3 = metricV31.CvssData.VectorString
+				severityV3, _ = types.NewSeverity(metricV31.CvssData.BaseSeverity)
+				if metricV31.Type == primaryType {
+					break
+				}
+			}
+		}
 
 		var references []string
-		for _, ref := range item.Cve.References.ReferenceDataList {
+		for _, ref := range cve.References {
 			references = append(references, ref.URL)
 		}
 
-		var (
-			description string
-		)
-		for _, d := range item.Cve.Description.DescriptionDataList {
+		var description string
+		for _, d := range cve.Descriptions {
 			if d.Value != "" {
 				description = d.Value
 				break
 			}
 		}
 		var cweIDs []string
-		for _, data := range item.Cve.ProblemType.ProblemTypeData {
+		for _, data := range cve.Weaknesses {
 			for _, desc := range data.Description {
-				if !strings.HasPrefix(desc.Value, "CWE") {
+				if !strings.HasPrefix(desc.Value, "CWE") || data.Type != primaryType {
 					continue
 				}
 				cweIDs = append(cweIDs, desc.Value)
 			}
 		}
 
-		publishedDate, _ := time.Parse("2006-01-02T15:04Z", item.PublishedDate)
-		lastModifiedDate, _ := time.Parse("2006-01-02T15:04Z", item.LastModifiedDate)
+		publishedDate, _ := time.Parse("2006-01-02T15:04:05", cve.Published)
+		lastModifiedDate, _ := time.Parse("2006-01-02T15:04:05", cve.LastModified)
 
 		vuln := types.VulnerabilityDetail{
-			CvssScore:        item.Impact.BaseMetricV2.CvssV2.BaseScore,
-			CvssVector:       item.Impact.BaseMetricV2.CvssV2.VectorString,
-			CvssScoreV3:      item.Impact.BaseMetricV3.CvssV3.BaseScore,
-			CvssVectorV3:     item.Impact.BaseMetricV3.CvssV3.VectorString,
+			CvssScore:        cvssScore,
+			CvssVector:       cvssVector,
+			CvssScoreV3:      cvssScoreV3,
+			CvssVectorV3:     CvssVectorV3,
 			Severity:         severity,
 			SeverityV3:       severityV3,
 			CweIDs:           cweIDs,
@@ -120,10 +141,10 @@ func (vs VulnSrc) commit(tx *bolt.Tx, items []Item) error {
 	return nil
 }
 
-func (vs VulnSrc) save(items []Item) error {
+func (vs VulnSrc) save(cves []Cve) error {
 	log.Println("NVD batch update")
 	err := vs.dbc.BatchUpdate(func(tx *bolt.Tx) error {
-		return vs.commit(tx, items)
+		return vs.commit(tx, cves)
 	})
 	if err != nil {
 		return xerrors.Errorf("error in batch update: %w", err)
--- pkg/vulnsrc/nvd/types.go
+++ pkg/vulnsrc/nvd/types.go
@@ -1,79 +1,90 @@
 package nvd
 
-type NVD struct {
-	CVEItems []Item `json:"CVE_Items"`
-}
-
-type Item struct {
-	Cve              Cve
-	Impact           Impact
-	LastModifiedDate string `json:"lastModifiedDate"`
-	PublishedDate    string `json:"publishedDate"`
-}
-
 type Cve struct {
-	Meta        Meta `json:"CVE_data_meta"`
-	References  References
-	Description Description
-	ProblemType ProblemType
-}
-
-type Meta struct {
-	ID string
-}
-
-type Impact struct {
-	BaseMetricV2 BaseMetricV2
-	BaseMetricV3 BaseMetricV3
-}
-
-type BaseMetricV2 struct {
-	CvssV2   CvssV2
-	Severity string
-}
-
-type CvssV2 struct {
-	BaseScore    float64 `json:"baseScore"`
-	VectorString string  `json:"vectorString"`
-}
-
-type BaseMetricV3 struct {
-	CvssV3 CvssV3
-}
-
-type CvssV3 struct {
-	BaseScore    float64 `json:"baseScore"`
-	BaseSeverity string
-	VectorString string `json:"vectorString"`
-}
-
-type References struct {
-	ReferenceDataList []ReferenceData `json:"reference_data"`
-}
-type ReferenceData struct {
-	Name      string
-	Refsource string
-	URL       string
+	ID               string          `json:"id"`
+	SourceIdentifier string          `json:"sourceIdentifier"`
+	Published        string          `json:"published"`
+	LastModified     string          `json:"lastModified"`
+	VulnStatus       string          `json:"vulnStatus"`
+	Descriptions     []Description   `json:"descriptions"`
+	Metrics          Metrics         `json:"metrics"`
+	Weaknesses       []Weakness      `json:"weaknesses"`
+	Configurations   []Configuration `json:"configurations"`
+	References       []Reference     `json:"references"`
 }
 
 type Description struct {
-	DescriptionDataList []DescriptionData `json:"description_data"`
+	Lang  string `json:"lang"`
+	Value string `json:"value"`
 }
 
-type DescriptionData struct {
-	Lang  string
-	Value string
+type Metrics struct {
+	CvssMetricV31 []CvssMetricV31 `json:"cvssMetricV31"`
+	CvssMetricV2  []CvssMetricV2  `json:"cvssMetricV2"`
 }
 
-type ProblemType struct {
-	ProblemTypeData []ProblemTypeData `json:"problemtype_data"`
+type CvssMetricV31 struct {
+	Source              string   `json:"source"`
+	Type                string   `json:"type"`
+	CvssData            CvssData `json:"cvssData"`
+	ExploitabilityScore float64  `json:"exploitabilityScore"`
+	ImpactScore         float64  `json:"impactScore"`
 }
 
-type ProblemTypeData struct {
-	Description []ProblemTypeDataDescription
+type CvssMetricV2 struct {
+	Source                  string   `json:"source"`
+	Type                    string   `json:"type"`
+	CvssData                CvssData `json:"cvssData"`
+	BaseSeverity            string   `json:"baseSeverity"`
+	ExploitabilityScore     float64  `json:"exploitabilityScore"`
+	ImpactScore             float64  `json:"impactScore"`
+	AcInsufInfo             bool     `json:"acInsufInfo"`
+	ObtainAllPrivilege      bool     `json:"obtainAllPrivilege"`
+	ObtainUserPrivilege     bool     `json:"obtainUserPrivilege"`
+	ObtainOtherPrivilege    bool     `json:"obtainOtherPrivilege"`
+	UserInteractionRequired bool     `json:"userInteractionRequired"`
 }
 
-type ProblemTypeDataDescription struct {
-	Lang  string
-	Value string
+type CvssData struct {
+	Version               string  `json:"version"`
+	VectorString          string  `json:"vectorString"`
+	AttackVector          string  `json:"attackVector"`
+	AttackComplexity      string  `json:"attackComplexity"`
+	PrivilegesRequired    string  `json:"privilegesRequired"`
+	UserInteraction       string  `json:"userInteraction"`
+	Scope                 string  `json:"scope"`
+	ConfidentialityImpact string  `json:"confidentialityImpact"`
+	IntegrityImpact       string  `json:"integrityImpact"`
+	AvailabilityImpact    string  `json:"availabilityImpact"`
+	BaseScore             float64 `json:"baseScore"`
+	BaseSeverity          string  `json:"baseSeverity"`
+}
+
+type Weakness struct {
+	Source      string        `json:"source"`
+	Type        string        `json:"type"`
+	Description []Description `json:"description"`
+}
+
+type Configuration struct {
+	Nodes []Node `json:"nodes"`
+}
+
+type Node struct {
+	Operator string     `json:"operator"`
+	Negate   bool       `json:"negate"`
+	CpeMatch []CpeMatch `json:"cpeMatch"`
+}
+type CpeMatch struct {
+	Vulnerable            bool   `json:"vulnerable"`
+	Criteria              string `json:"criteria"`
+	VersionEndExcluding   string `json:"versionEndExcluding"`
+	MatchCriteriaID       string `json:"matchCriteriaId"`
+	VersionStartIncluding string `json:"versionStartIncluding,omitempty"`
+}
+
+type Reference struct {
+	URL    string   `json:"url"`
+	Source string   `json:"source"`
+	Tags   []string `json:"tags"`
 }
