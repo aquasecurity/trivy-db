51be8d9236a79cd6e19f5bf09d518c795a7abe0c
DmitriyLewen <91113035+DmitriyLewen@users.noreply.github.com>
2023-12-19 01:30:30 +0600

    refactor(nvd): migrate to API 2.0 (#374)
    Signed-off-by: knqyf263 <knqyf263@gmail.com>
    Co-authored-by: knqyf263 <knqyf263@gmail.com>

--- pkg/vulnsrc/nvd/nvd.go
+++ pkg/vulnsrc/nvd/nvd.go
@@ -9,6 +9,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/samber/lo"
 	bolt "go.etcd.io/bbolt"
 	"golang.org/x/xerrors"
 
@@ -20,7 +21,8 @@ import (
 
 const (
 	vulnListDir = "vuln-list-nvd"
-	feedDir     = "feed"
+	apiDir      = "api"
+	nvdSource   = "nvd@nist.gov"
 )
 
 type VulnSrc struct {
@@ -38,55 +40,54 @@ func (vs VulnSrc) Name() types.SourceID {
 }
 
 func (vs VulnSrc) Update(dir string) error {
-	rootDir := filepath.Join(dir, vulnListDir, feedDir)
+	rootDir := filepath.Join(dir, vulnListDir, apiDir)
 
-	var items []Item
+	var cves []Cve
 	buffer := &bytes.Buffer{}
 	err := utils.FileWalk(rootDir, func(r io.Reader, _ string) error {
-		item := Item{}
+		cve := Cve{}
 		if _, err := buffer.ReadFrom(r); err != nil {
 			return xerrors.Errorf("failed to read file: %w", err)
 		}
-		if err := json.Unmarshal(buffer.Bytes(), &item); err != nil {
+		if err := json.Unmarshal(buffer.Bytes(), &cve); err != nil {
 			return xerrors.Errorf("failed to decode NVD JSON: %w", err)
 		}
 		buffer.Reset()
-		items = append(items, item)
+		cves = append(cves, cve)
 		return nil
 	})
 	if err != nil {
 		return xerrors.Errorf("error in NVD walk: %w", err)
 	}
 
-	if err = vs.save(items); err != nil {
+	if err = vs.save(cves); err != nil {
 		return xerrors.Errorf("error in NVD save: %w", err)
 	}
 
 	return nil
 }
 
-func (vs VulnSrc) commit(tx *bolt.Tx, items []Item) error {
-	for _, item := range items {
-		cveID := item.Cve.Meta.ID
-		severity, _ := types.NewSeverity(item.Impact.BaseMetricV2.Severity)
-		severityV3, _ := types.NewSeverity(item.Impact.BaseMetricV3.CvssV3.BaseSeverity)
+func (vs VulnSrc) commit(tx *bolt.Tx, cves []Cve) error {
+	for _, cve := range cves {
+		cveID := cve.ID
+
+		cvssScore, cvssVector, severity := getCvssV2(cve.Metrics.CvssMetricV2)
+		cvssScoreV3, cvssVectorV3, severityV3 := getCvssV3(cve.Metrics.CvssMetricV31, cve.Metrics.CvssMetricV30)
 
 		var references []string
-		for _, ref := range item.Cve.References.ReferenceDataList {
+		for _, ref := range cve.References {
 			references = append(references, ref.URL)
 		}
 
-		var (
-			description string
-		)
-		for _, d := range item.Cve.Description.DescriptionDataList {
+		var description string
+		for _, d := range cve.Descriptions {
 			if d.Value != "" {
 				description = d.Value
 				break
 			}
 		}
 		var cweIDs []string
-		for _, data := range item.Cve.ProblemType.ProblemTypeData {
+		for _, data := range cve.Weaknesses {
 			for _, desc := range data.Description {
 				if !strings.HasPrefix(desc.Value, "CWE") {
 					continue
@@ -95,17 +96,17 @@ func (vs VulnSrc) commit(tx *bolt.Tx, items []Item) error {
 			}
 		}
 
-		publishedDate, _ := time.Parse("2006-01-02T15:04Z", item.PublishedDate)
-		lastModifiedDate, _ := time.Parse("2006-01-02T15:04Z", item.LastModifiedDate)
+		publishedDate, _ := time.Parse("2006-01-02T15:04:05", cve.Published)
+		lastModifiedDate, _ := time.Parse("2006-01-02T15:04:05", cve.LastModified)
 
 		vuln := types.VulnerabilityDetail{
-			CvssScore:        item.Impact.BaseMetricV2.CvssV2.BaseScore,
-			CvssVector:       item.Impact.BaseMetricV2.CvssV2.VectorString,
-			CvssScoreV3:      item.Impact.BaseMetricV3.CvssV3.BaseScore,
-			CvssVectorV3:     item.Impact.BaseMetricV3.CvssV3.VectorString,
+			CvssScore:        cvssScore,
+			CvssVector:       cvssVector,
+			CvssScoreV3:      cvssScoreV3,
+			CvssVectorV3:     cvssVectorV3,
 			Severity:         severity,
 			SeverityV3:       severityV3,
-			CweIDs:           cweIDs,
+			CweIDs:           lo.Uniq(cweIDs),
 			References:       references,
 			Title:            "",
 			Description:      description,
@@ -120,13 +121,42 @@ func (vs VulnSrc) commit(tx *bolt.Tx, items []Item) error {
 	return nil
 }
 
-func (vs VulnSrc) save(items []Item) error {
+func (vs VulnSrc) save(cves []Cve) error {
 	log.Println("NVD batch update")
 	err := vs.dbc.BatchUpdate(func(tx *bolt.Tx) error {
-		return vs.commit(tx, items)
+		return vs.commit(tx, cves)
 	})
 	if err != nil {
 		return xerrors.Errorf("error in batch update: %w", err)
 	}
 	return nil
+}
+
+// getCvssV2 selects vector, score and severity from V2 metrics
+func getCvssV2(metricsV2 []CvssMetricV2) (score float64, vector string, severity types.Severity) {
+	for _, metricV2 := range metricsV2 {
+		// save only NVD metric
+		if metricV2.Source == nvdSource {
+			score = metricV2.CvssData.BaseScore
+			vector = metricV2.CvssData.VectorString
+			severity, _ = types.NewSeverity(metricV2.BaseSeverity)
+			return
+		}
+	}
+	return
+}
+
+// getCvssV3 selects vector, score and severity from V3* metrics
+func getCvssV3(metricsV31, metricsV30 []CvssMetricV3) (score float64, vector string, severity types.Severity) {
+	// order: v3.1 metrics => v3.0 metrics
+	// save the first NVD metric
+	for _, metricV3 := range append(metricsV31, metricsV30...) {
+		if metricV3.Source == nvdSource {
+			score = metricV3.CvssData.BaseScore
+			vector = metricV3.CvssData.VectorString
+			severity, _ = types.NewSeverity(metricV3.CvssData.BaseSeverity)
+			return
+		}
+	}
+	return
 }
--- pkg/vulnsrc/nvd/types.go
+++ pkg/vulnsrc/nvd/types.go
@@ -1,79 +1,69 @@
 package nvd
 
-type NVD struct {
-	CVEItems []Item `json:"CVE_Items"`
-}
-
-type Item struct {
-	Cve              Cve
-	Impact           Impact
-	LastModifiedDate string `json:"lastModifiedDate"`
-	PublishedDate    string `json:"publishedDate"`
-}
-
+// Cve is based on https://csrc.nist.gov/schema/nvd/api/2.0/cve_api_json_2.0.schema (see `cve_item`)
 type Cve struct {
-	Meta        Meta `json:"CVE_data_meta"`
-	References  References
-	Description Description
-	ProblemType ProblemType
+	ID           string       `json:"id"`
+	Published    string       `json:"published"`
+	LastModified string       `json:"lastModified"`
+	Descriptions []LangString `json:"descriptions"`
+	Metrics      Metrics      `json:"metrics,omitempty"`
+	Weaknesses   []Weakness   `json:"weaknesses,omitempty"`
+	References   []Reference  `json:"references"`
 }
 
-type Meta struct {
-	ID string
+type LangString struct {
+	Lang  string `json:"lang"`
+	Value string `json:"value"`
 }
 
-type Impact struct {
-	BaseMetricV2 BaseMetricV2
-	BaseMetricV3 BaseMetricV3
+type Reference struct {
+	URL    string   `json:"url"`
+	Source string   `json:"source,omitempty"`
+	Tags   []string `json:"tags,omitempty"`
 }
 
-type BaseMetricV2 struct {
-	CvssV2   CvssV2
-	Severity string
+type Metrics struct {
+	CvssMetricV31 []CvssMetricV3 `json:"cvssMetricV31,omitempty"`
+	CvssMetricV30 []CvssMetricV3 `json:"cvssMetricV30,omitempty"`
+	CvssMetricV2  []CvssMetricV2 `json:"cvssMetricV2,omitempty"`
 }
 
-type CvssV2 struct {
-	BaseScore    float64 `json:"baseScore"`
+// CvssMetricV3 is based on https://csrc.nist.gov/schema/nvd/api/2.0/cve_api_json_2.0.schema.
+// v3.0 and v3.1 have only one difference: `cvssData`.
+// But we can use `cvssData` v3.0 for v3.1 (see below).
+// So we can use the same structure for v3.0 and v3.1.
+type CvssMetricV3 struct {
+	Source   string      `json:"source"`
+	Type     string      `json:"type"`
+	CvssData CvssDataV30 `json:"cvssData"`
+}
+
+// CvssDataV30 is based on https://csrc.nist.gov/schema/nvd/api/2.0/external/cvss-v3.0.json
+// v3.0 and v3.1 have only one difference: `patterns` for `vectorString`.
+// So we can use version 3.0 for version 3.1.
+type CvssDataV30 struct {
+	Version      string  `json:"version"`
 	VectorString string  `json:"vectorString"`
-}
-
-type BaseMetricV3 struct {
-	CvssV3 CvssV3
-}
-
-type CvssV3 struct {
 	BaseScore    float64 `json:"baseScore"`
-	BaseSeverity string
-	VectorString string `json:"vectorString"`
+	BaseSeverity string  `json:"baseSeverity"`
 }
 
-type References struct {
-	ReferenceDataList []ReferenceData `json:"reference_data"`
-}
-type ReferenceData struct {
-	Name      string
-	Refsource string
-	URL       string
+type CvssMetricV2 struct {
+	Source       string      `json:"source"`
+	Type         string      `json:"type"`
+	CvssData     CvssDataV20 `json:"cvssData"`
+	BaseSeverity string      `json:"baseSeverity,omitempty"`
 }
 
-type Description struct {
-	DescriptionDataList []DescriptionData `json:"description_data"`
+// CvssDataV20 is based on https://csrc.nist.gov/schema/nvd/api/2.0/external/cvss-v2.0.json
+type CvssDataV20 struct {
+	Version      string  `json:"version"`
+	VectorString string  `json:"vectorString"`
+	BaseScore    float64 `json:"baseScore"`
 }
 
-type DescriptionData struct {
-	Lang  string
-	Value string
-}
-
-type ProblemType struct {
-	ProblemTypeData []ProblemTypeData `json:"problemtype_data"`
-}
-
-type ProblemTypeData struct {
-	Description []ProblemTypeDataDescription
-}
-
-type ProblemTypeDataDescription struct {
-	Lang  string
-	Value string
+type Weakness struct {
+	Source      string       `json:"source"`
+	Type        string       `json:"type"`
+	Description []LangString `json:"description"`
 }
