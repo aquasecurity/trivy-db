bbb59d74305d29ca211691043632cfbc1fc06386
cleanstart-community-admin <community-admin@cleanstart.com>
2026-03-12 09:54:01 +0000

    feat: add CleanStart vulnerability data source

--- pkg/vulnsrc/vulnsrc.go
+++ pkg/vulnsrc/vulnsrc.go
@@ -11,6 +11,7 @@ import (
 	"github.com/aquasecurity/trivy-db/pkg/vulnsrc/bottlerocket"
 	"github.com/aquasecurity/trivy-db/pkg/vulnsrc/bundler"
 	"github.com/aquasecurity/trivy-db/pkg/vulnsrc/chainguard"
+	"github.com/aquasecurity/trivy-db/pkg/vulnsrc/cleanstart"
 	"github.com/aquasecurity/trivy-db/pkg/vulnsrc/composer"
 	"github.com/aquasecurity/trivy-db/pkg/vulnsrc/debian"
 	"github.com/aquasecurity/trivy-db/pkg/vulnsrc/echo"
@@ -63,6 +64,7 @@ var (
 		azure.NewVulnSrc(azure.Mariner),
 		wolfi.NewVulnSrc(),
 		chainguard.NewVulnSrc(),
+		cleanstart.NewVulnSrc(),
 		bitnami.NewVulnSrc(),
 		echo.NewVulnSrc(),
 		minimos.NewVulnSrc(),
