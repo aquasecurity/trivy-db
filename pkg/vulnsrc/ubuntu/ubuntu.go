6bcbb43988306c6d89e512a7b1b523fa3c5cc883
Jonathan Machado <jonathan.machado@outlook.com>
2026-05-14 15:12:42 -0400

    Remove non EOL ESM and FIPS entries in ubuntu.go
    Removed non EOL ESM and FIPS entries for Ubuntu versions.

--- pkg/vulnsrc/ubuntu/ubuntu.go
+++ pkg/vulnsrc/ubuntu/ubuntu.go
@@ -64,22 +64,6 @@ var (
 		"esm-apps/bionic":         "18.04-ESM",
 		"esm-infra/focal":         "20.04-ESM",
 		"esm-apps/focal":          "20.04-ESM",
-		"esm-infra/jammy":         "22.04-ESM",
-		"esm-apps/jammy":          "22.04-ESM",
-		"esm-infra/noble":         "24.04-ESM",
-		"esm-apps/noble":          "24.04-ESM",
-		"esm-infra/resolute":      "26.04-ESM",
-		"esm-apps/resolute":       "26.04-ESM",
-		// FIPS
-		"fips/xenial":         "16.04",
-		"fips-updates/xenial": "16.04",
-		"fips/bionic":         "18.04",
-		"fips-updates/bionic": "18.04",
-		"fips/focal":          "20.04",
-		"fips-updates/focal":  "20.04",
-		"fips-updates/jammy":  "22.04",
-		"fips-preview/jammy":  "22.04",
-		"fips-updates/noble":  "24.04",
 	}
 
 	source = types.DataSource{
